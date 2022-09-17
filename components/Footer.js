d8ee75289c0219d0031c37f0ad340bac31fc3e4d
ashtonw7 <ashton.winters@aya.yale.edu>
2022-09-17 15:34:33 -0400

    centered footer always

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 
     return (
-        <footer className="drop-shadow-md bg-black w-full phone:h-auto mt-10 phone:px-5 text-white flex items-center justify-center centerfooter:justify-end">
+        <footer className="drop-shadow-md bg-black w-full phone:h-auto mt-10 phone:px-5 text-white flex items-center justify-center">
             <span className="mt-10 mb-10 centerfooter:mb-10 px-10 phone:px-0 centerfooter:px-0 text-center">© 2022 Writing with No Purpose. All content by Ashton Winters. All Rights Reserved.</span>
         </footer>
     )
