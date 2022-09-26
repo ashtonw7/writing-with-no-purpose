93ecd46780d6e0139de3320fc6792331dba4fa1f
ashtonw7 <ashton.winters@aya.yale.edu>
2022-09-26 01:44:29 -0400

    fixed borders and margin on readmore

--- components/ReadMore.js
+++ components/ReadMore.js
@@ -1,12 +1,30 @@
 import ArticleCard from "./ArticleCard"
 
 export default function ReadMore({ randFiles }) {
+    let randCards = [];
+    let posts = Object.values(randFiles)
+    console.log(posts)
+    console.log(Object.keys(randFiles)[1])
+    for(let i=0; i<Object.keys(randFiles).length; i++){
+        console.log(randFiles)
+        let post = randFiles[Object.keys(randFiles)[i]]
+        let slug = post.slug;
+        let frontmatter = post.frontmatter;
+
+        randCards.push(<ArticleCard key={slug} slug={slug} title={frontmatter.title} author={frontmatter.author} quote={frontmatter.quote} date={frontmatter.date} currArticle={i + 1} totalCount={posts.length} />);
+    }
+
     return (
-        <div className='flex flex-col justify-center mt-10 border-t'>
-            <h2 className="text-3xl phone:text-5xl text-center mt-10 underline decoration-2 underline-offset-[6px] mb-10">Read More</h2>
+        <div className='flex flex-col justify-center mt-10 mb-10'>
+            <h2 className="text-3xl phone:text-5xl text-center underline decoration-2 underline-offset-[6px] mb-10">Read More</h2>
+            {randCards}
+
+            {/*
             {Object.keys(randFiles).map((item => (
                 <ArticleCard key={randFiles[item].slug} slug={randFiles[item].slug} title={randFiles[item].frontmatter.title} author={randFiles[item].frontmatter.author} quote={randFiles[item].frontmatter.quote} date={randFiles[item].frontmatter.date} />
             )))}
+            */ }
+
         </div>
     )
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -81,7 +81,7 @@ export default function PostPage({ frontmatter, content, randFiles }) {
         
         <div className='flex flex-col justify-center w-full'>
 
-        <div className='flex justify-center mb-5'>
+        <div className='flex justify-center '>
             <HeadTag title={frontmatter.title + ' | Writing with No Purpose'} />
             <div className='items-center w-4/5'>
               <div className='flex justify-center mt-5'>
@@ -89,7 +89,7 @@ export default function PostPage({ frontmatter, content, randFiles }) {
               </div>
               <h1 className='text-4xl phone:text-6xl font-dominique text-center'>{frontmatter.title.toLowerCase()}</h1>
               <p className='phone:text-xl text-center mb-10 text-gray-500 font-tinos'>{frontmatter.date}</p>
-              <article className="article text-xl phone:text-2xl [&>*]:mb-5 font-tinos">
+              <article className="border-b pb-10 article text-xl phone:text-2xl [&>*]:mb-5 font-tinos">
                 <ReactMarkdown>{content}</ReactMarkdown>
               </article>
             </div>
