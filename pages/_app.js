2b15b46ea9919c1dac15b8dbec4d27c2efe8dc6f
ashtonw7 <ashton.winters@aya.yale.edu>
2022-09-29 21:28:21 -0400

    added microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,13 @@ function Application({ Component, pageProps }) {
         <html lang="en" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "dvgo90m9vk");`}
+        </script>
       </Helmet>
       <div className="flex flex-col h-screen">
         <Header />
